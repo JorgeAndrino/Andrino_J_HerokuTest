972b22cd575a26fed50771cc45fdf6ef39677fe6
Andrino Van Der Beek <j_andrinovanderbeek@tss-hr420-dm20.fc.ca>
2019-03-14 17:47:35 -0400

    update

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = {
+    name: "George",
+    role: "Hero",
+
+    user1: {
+        name: "user1",
+        role: "designer",
+        bio: "Cool dude",
+        bioPic: ""
+    },
+
+    user2: {
+        name: "user2",
+        role: "developer",
+        bio: "Cool chick",
+        bioPic: ""
+    }
+
+}
+
+module.exports = data;
