7539e1092badd19b4df8fbcf6e513c08f236b94d
Andrino Van Der Beek <j_andrinovanderbeek@tss-hr420-dm20.fc.ca>
2019-03-14 17:05:37 -0400

    Update

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// use the pulic folder for any web assets (css js, etc)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
